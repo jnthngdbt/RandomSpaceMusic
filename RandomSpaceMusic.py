6682757a0091e467cd2184a5135f4e7cc9f8e18d
Jonathan Godbout <jon.godbout@gmail.com>
2019-01-11 23:19:56 -0500

    Creating simple melody
    - each note mode is created using Butterworth bandpass filter on white noise
    - all the music theory stuff is in getNoteFrequency(k), using 440Hz as root pitch

--- RandomSpaceMusic.py
+++ RandomSpaceMusic.py
@@ -2,26 +2,76 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
+import scipy.signal
 
 matplotlib.style.use('dark_background')
 
 fs = 44100 # Hz
 dt = 1/fs
-T = 4 # s
-t = np.arange(0,T,dt)
-N = len(t)
-A = 0.8 * 2**16 # will be int16
 
-fx = 5000 # Hz
-x = A * np.sin(2*np.pi*fx*t)
+def getTimeDomain(T):
+  return np.arange(0,T,dt)
 
-scipy.io.wavfile.write("sine.wav", fs, x.astype(np.int16))
+def plotSound(x):
+  T = dt*len(x)
+  t = getTimeDomain(T)
 
-plt.plot(t,x,'.-',markersize=3,linewidth=1)
-plt.xlabel('time (s)')
-plt.ylabel('sound')
-snip = 0.01
-plt.xlim([0, snip])
-plt.title('Showing {}s/{}s of {}Hz signal ({}Hz)'.format(snip, T, fx, fs))
+  plt.figure()
+  plt.plot(t, x, '.-', markersize=3, linewidth=1)
+  plt.xlabel('time (s)')
+  plt.ylabel('sound')
+
+  f = np.fft.fftfreq(len(x), dt)
+  F = np.fft.fft(x)
+
+  plt.figure()
+  plt.plot(f, np.real(F), '.-', markersize=3, linewidth=1)
+  plt.xlabel('frequency (Hz)')
+  plt.ylabel('amplitude')
+  plt.title('FFT')
+
+def writeWav(x, name, volume=0.9):
+  A = volume * 2**15 # will be int16; 2^16/2, since signed
+  x *= A/np.max(x)
+  scipy.io.wavfile.write(name, fs, x.astype(np.int16))
+
+def generateSine(T=2, fx=1000):
+  t = getTimeDomain(T)
+  x = np.sin(2*np.pi*fx*t)
+  return x
+
+def generateNoiseMode(T=2, fx=1000):
+  t = getTimeDomain(T)
+  N = len(t)
+
+  hfs = fs/2
+  df = fx/30
+  lf = (fx-df)/hfs
+  hf = (fx+df)/hfs
+  order = 10
+  sos = scipy.signal.butter(order, [lf, hf], 'bandpass', output='sos')
+
+  r = np.random.rand(N)
+  x = scipy.signal.sosfilt(sos, r)
+
+  return x
+
+# s = generateSine(T=4, fx=5000)
+
+def getNoteFrequency(k):
+  return 2 ** (k/12) * 440
+
+k0 = 0
+f0 = getNoteFrequency(k0)
+f1 = getNoteFrequency(k0+4)
+f2 = getNoteFrequency(k0+5)
+f3 = getNoteFrequency(k0+7)
+
+s1 = generateNoiseMode(T=4, fx=f0) + generateNoiseMode(T=4, fx=f1)
+s2 = generateNoiseMode(T=4, fx=f0) + generateNoiseMode(T=4, fx=f2)
+s3 = generateNoiseMode(T=4, fx=f0) + generateNoiseMode(T=4, fx=f3)
+s = np.concatenate((s1,s2,s3))
+writeWav(s, "noisemode.wav")
+# plotSound(s)
 
 plt.show()
