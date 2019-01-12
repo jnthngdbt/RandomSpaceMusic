f4866c4d3b0904ec974894594f13f2728c9a8d5d
Jonathan Godbout <jon.godbout@gmail.com>
2019-01-12 15:10:40 -0500

    Using subplots in a single figure

--- RandomSpaceMusicTest.py
+++ RandomSpaceMusicTest.py
@@ -5,6 +5,8 @@ def plotSound(x):
   t = getTimeDomain(T)
 
   plt.figure()
+
+  plt.subplot(1,2,1)
   plt.plot(t, x, '.-', markersize=3, linewidth=1)
   plt.xlabel('time (s)')
   plt.ylabel('sound')
@@ -12,14 +14,14 @@ def plotSound(x):
   f = np.fft.fftfreq(len(x), dt)
   F = np.fft.fft(x)
 
-  plt.figure()
-  plt.plot(f, np.real(F), '.-', markersize=3, linewidth=1)
+  plt.subplot(1,2,2)
+  plt.plot(f, np.abs(F), '.-', markersize=3, linewidth=1)
   plt.xlabel('frequency (Hz)')
   plt.ylabel('amplitude')
   plt.title('FFT')
 
 def testNoiseSignal():
-  x = generateNoiseTone(T=0.05, fx=5000)
+  x = generateNoiseTone(T=0.1, fx=5000)
   plotSound(x)
 
 def testSong():
