d4f160e2c807430e54b1d043eebdb5f70e41f8d0
Jonathan Godbout <jon.godbout@gmail.com>
2021-03-12 15:06:06 -0500

    Added chord sound type: sine or peak

--- chord.py
+++ chord.py
@@ -5,13 +5,13 @@ import note
 
 from constants import *
 
-def major(T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[]):
-  return signal(scales["major"], T, root, notes, Tf, amps)
+def major(T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[], type=None):
+  return signal(scales["major"], T, root, notes, Tf, amps, type)
 
-def minor(T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[]):
-  return signal(scales["minor"], T, root, notes, Tf, amps)
+def minor(T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[], type=None):
+  return signal(scales["minor"], T, root, notes, Tf, amps, type)
   
-def signal(scale, T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[]):
+def signal(scale, T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[], type=None):
   if len(amps) == 0: amps = np.ones(len(notes))
   elif len(amps) != len(notes): raise NameError("Number of amps must match number of notes.")
 
@@ -19,7 +19,10 @@ def signal(scale, T=2, root=0, notes=[1,3,5,8], Tf=0, amps=[]):
   for i, n in enumerate(notes):
     k = midi.number(root, n, scale)
     f = midi.freq(k)
-    s += amps[i] * note.sine(T, f, Tf)
+    if type == None or type == 'sine':
+      s += amps[i] * note.sine(T=T, fx=f, Tf=Tf)
+    elif type == 'peak':
+      s += amps[i] * note.peak(T=T, fx=f, Tf=Tf)
 
   s = note.normalize(s)
   return s
